8e019905d7312d127127dae0dedfc644981c49de
Nicod Gaetan <nicod.ga@gmail.com>
2020-03-24 12:27:53 +0100

    add missing grant_type refresh_code

--- lib/src/main/java/com/sweetzpot/stravazpot/authenticaton/request/AuthenticationRefreshRequest.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/authenticaton/request/AuthenticationRefreshRequest.java
@@ -1,11 +1,11 @@
 package com.sweetzpot.stravazpot.authenticaton.request;
 
+import retrofit2.Call;
 import com.sweetzpot.stravazpot.authenticaton.api.AuthenticationAPI;
 import com.sweetzpot.stravazpot.authenticaton.model.AppCredentials;
 import com.sweetzpot.stravazpot.authenticaton.model.LoginResult;
 import com.sweetzpot.stravazpot.authenticaton.rest.AuthenticationRest;
 
-import retrofit2.Call;
 
 public class AuthenticationRefreshRequest {
 
@@ -26,7 +26,7 @@ public class AuthenticationRefreshRequest {
     }
 
     public LoginResult execute() {
-        Call<LoginResult> call = restService.refreshToken(appCredentials.getClientID(), appCredentials.getClientSecret(), refreshToken);
+        Call<LoginResult> call = restService.refreshToken(appCredentials.getClientID(), appCredentials.getClientSecret(), refreshToken, "refresh_token");
         return api.execute(call);
     }
 }
--- lib/src/main/java/com/sweetzpot/stravazpot/authenticaton/rest/AuthenticationRest.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/authenticaton/rest/AuthenticationRest.java
@@ -21,7 +21,8 @@ public interface AuthenticationRest {
     Call<LoginResult> refreshToken(
             @Field("client_id") int clientID,
             @Field("client_secret") String clientSecret,
-            @Field("refresh_token") String refreshToken);
+            @Field("refresh_token") String refreshToken,
+            @Field("grant_type") String grantType);
 
     @POST("/oauth/deauthorize")
     Call<Void> deauthorize();
